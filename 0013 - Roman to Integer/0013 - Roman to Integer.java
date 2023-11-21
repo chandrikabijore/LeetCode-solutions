6e4e615b39cde1de07563d97e0b3277c6a232d80
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-21 19:31:52 +0530

    Update 0013 - Roman to Integer.java

--- 0013 - Roman to Integer/0013 - Roman to Integer.java
+++ 0013 - Roman to Integer/0013 - Roman to Integer.java
@@ -1,1 +1,30 @@
+class Solution { 
 
+public int romanToInt(String s) {
+    // Create a HashMap to store the values of Roman numerals
+    HashMap<Character, Integer> romanMap = new HashMap<>();
+    romanMap.put('I', 1);
+    romanMap.put('V', 5);
+    romanMap.put('X', 10);
+    romanMap.put('L', 50);
+    romanMap.put('C', 100);
+    romanMap.put('D', 500);
+    romanMap.put('M', 1000);
+    
+    int result = 0;
+    int prevValue = 0;
+    
+    for (int i = s.length() - 1; i >= 0; i--) {
+        int currValue = romanMap.get(s.charAt(i));
+        if (currValue < prevValue) {
+            result -= currValue;
+        } else {
+            result += currValue;
+        }
+        prevValue = currValue;
+    }
+    
+    return result;
+}
+
+}
