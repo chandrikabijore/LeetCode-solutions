14e01e4e94973a508bf832969d9398af6bff618d
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-21 19:07:47 +0530

    Time: 7ms (21.84%) Space: 43.12MB (22.01%) - LeetHub

--- 0009 - Palindrome Number/9. Palindrome Number.java
+++ 0009 - Palindrome Number/9. Palindrome Number.java
@@ -1,1 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    // Convert the integer to a string
+    String str = Integer.toString(x);
+    
+    // Create a StringBuilder to reverse the string
+    StringBuilder reversed = new StringBuilder(str);
+    reversed.reverse();
+    
+    // Compare the original string with the reversed string
+    return str.equals(reversed.toString());
+}
+
+}
 
