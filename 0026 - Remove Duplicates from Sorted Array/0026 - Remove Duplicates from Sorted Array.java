bcdaca03c36063dde8054edb3d1fc3b7c2f0657f
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-23 05:20:14 +0530

    Time: 10ms (90.00%), Space: 44.30MB (10.03%) - LeetHub

--- 0026 - Remove Duplicates from Sorted Array/0026 - Remove Duplicates from Sorted Array.java
+++ 0026 - Remove Duplicates from Sorted Array/0026 - Remove Duplicates from Sorted Array.java
@@ -1,5 +1,7 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
+class Solution 
+{
+    public int removeDuplicates(int[] nums) 
+    {
         int j = 0;
         for(int i = 0; i < nums.length; i++)
         {
