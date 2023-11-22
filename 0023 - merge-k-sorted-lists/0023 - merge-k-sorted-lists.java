add23c1061243a197012aa56c67164d4e426f75b
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-23 05:05:24 +0530

    Time: 8ms (28.13%), Space: 45.65MB (5.27%) - LeetHub

--- 0023 - merge-k-sorted-lists/0023 - merge-k-sorted-lists.java
+++ 0023 - merge-k-sorted-lists/0023 - merge-k-sorted-lists.java
@@ -6,8 +6,7 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+ * } */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         ArrayList<Integer> list = new ArrayList<>();
