8b2ea2a29432e9cfc31542fadadbdd761337b087
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-24 14:47:51 +0530

    Time: 16ms (82.00%), Space: 41.34MB (30.27%) - LeetHub

--- 0048 - Rotate Image/0048 - Rotate Image.java
+++ 0048 - Rotate Image/0048 - Rotate Image.java
@@ -1,7 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
-         transpose(matrix);
-        
+         transpose(matrix);        
          
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix.length/2;j++){
