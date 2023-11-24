d61ec3ae9329143ccd178d849722aa00cf5c22eb
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-24 22:54:10 +0530

    Create 49 - Group Anagrams.java

---
+++ 49 - Group Anagrams/49 - Group Anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        // System.out.println(map);
+        return new ArrayList<>(map.values());
+    }
+}
