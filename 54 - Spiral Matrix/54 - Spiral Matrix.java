c0f42af2f24a28e290743cade7064593921dcb9b
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-24 23:31:00 +0530

    81ms (78.00%), Space: 41.19MB (5.56%) - LeetHub

--- 54 - Spiral Matrix/54 - Spiral Matrix.java
+++ 54 - Spiral Matrix/54 - Spiral Matrix.java
@@ -1,2 +1,44 @@
-
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int topRow = 0;
+        int leftCol = 0;
+        int bottomRow = matrix.length-1;
+        int rightCol = matrix[0].length-1;
+        int count = 0;
+        int total = matrix.length * matrix[0].length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int dir = 0;
+        while(count < total){
+            if(dir == 0){
+            for(int i=leftCol;i<=rightCol;i++){
+                list.add(matrix[topRow][i]);
+                count++;
+            }
+                topRow++;
+            }
+            else if(dir == 1){
+            for(int i=topRow;i<=bottomRow;i++){
+                list.add(matrix[i][rightCol]);
+                count++;
+            }
+            rightCol--;
+            }else if(dir == 2){
+            for(int i=rightCol;i>=leftCol;i--){
+                count++;
+                list.add(matrix[bottomRow][i]);
+            }
+            bottomRow--;
+            }else if(dir == 3){
+            for(int i=bottomRow;i>=topRow;i--){
+               count++;
+                list.add(matrix[i][leftCol]);
+            }
+            leftCol++;
+            }
+            dir = (dir+1)%4;
+        }
+        System.out.println(list);
+        return list;
+    }
+}
 
