c99d359d8021ad586dcccbe244dfc5471d7e43db
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-25 11:03:52 +0530

    Time: 11ms (67.52%), Space: 40.51MB (8.75%) - LeetHub

--- 0119 - Pascal's Triangle II/0119 - Pascal's Triangle II.java
+++ 0119 - Pascal's Triangle II/0119 - Pascal's Triangle II.java
@@ -1,1 +1,30 @@
+// class Solution PascalTriangle{
+//     public List<Integer> getRow(int rowIndex) {
+        
+//     }
+// }
+import java.util.ArrayList;
+import java.util.List;
 
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+        }
+        return row;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int rowIndex1 = 3;
+        int rowIndex2 = 0;
+        int rowIndex3 = 1;
+
+        System.out.println(solution.getRow(rowIndex1)); // Output: [1, 3, 3, 1]
+        System.out.println(solution.getRow(rowIndex2)); // Output: [1]
+        System.out.println(solution.getRow(rowIndex3)); // Output: [1, 1]
+    }
+}
