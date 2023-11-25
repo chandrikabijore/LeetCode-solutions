2d4077d8f77255c3820b6039cde3a13c74d01da5
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-26 00:22:14 +0530

    Time: 13ms (51.03%),  43.90MB (21.00%) -LeetHub

---
+++ 0142 - Linked List Cycle II/142 - Linked List Cycle II.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> map = new HashSet<>();
+         
+        while(head != null){
+            if(map.contains(head)){
+                return head;
+            }else{
+                map.add(head);
+                head = head.next;
+            }
+        }
+        return null;
+    }
+}
