cc0797778e2f31a935b216446aded627b8a72732
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-25 12:28:22 +0530

    Time: 8ms (79.93%), Space: 44.34MB (61.25%) - LeetHub

--- 136 - Single Number/136 - Single Number.java
+++ 136 - Single Number/136 - Single Number.java
@@ -1,6 +1,7 @@
 class Solution 
 {
-    public int singleNumber(int[] nums) {
+    public int singleNumber(int[] nums) 
+    {
         int ans = 0;
         for(int i=0;i<nums.length;i++){
             ans ^= nums[i];
