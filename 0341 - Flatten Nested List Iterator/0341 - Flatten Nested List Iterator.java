e9ba7010a44ebfe06ec0f6c60fd7bddd92d54f77
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-28 21:43:24 +0530

    Time: 13ms (67.91%), Space: 44.53MB (37.65%) - LeetHub

---
+++ 0341 - Flatten Nested List Iterator/0341 - Flatten Nested List Iterator.java
@@ -0,0 +1,60 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+import java.util.*;
+
+public class NestedIterator implements Iterator<Integer> {
+    private Deque<NestedInteger> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new ArrayDeque<>();
+        // Push all elements in reverse order so that the top of the stack contains the first element.
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            if (stack.peek().isInteger()) {
+                return true;
+            } else {
+                List<NestedInteger> nestedList = stack.pop().getList();
+                // Push elements of the nested list in reverse order.
+                for (int i = nestedList.size() - 1; i >= 0; i--) {
+                    stack.push(nestedList.get(i));
+                }
+            }
+        }
+        return false;
+    }
+}
