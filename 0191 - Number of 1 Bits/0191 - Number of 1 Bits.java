5457952ac062e31d0ec838e585cc8b912d3a78d7
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-11-29 21:56:18 +0530

    Time: 7ms (90.00%), Space: 39.77MB (21.66%) LeetHub

--- 0191 - Number of 1 Bits/0191 - Number of 1 Bits.java
+++ 0191 - Number of 1 Bits/0191 - Number of 1 Bits.java
@@ -1,1 +1,20 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    // we can check the right most bit if it is one then 
+    // increment the count
+    // and right shift the number by 1
+    public int hammingWeight(int n) {
+          int count = 0;
+          
+          while(n !=0 ){
+              count += (n&1);
+              n = n>>>1;
+          }
+        return count;
+    }
+}
+
+
+
+
 
