93113b5b58097b7299d79731f76b183aee793118
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-06-07 09:25:47 +0530

    Time: 9 ms (24.67%), Space: 41.50 MB (83.38%) - LeetHub

--- 0409 - Longest Palindrome/0409 - Longest Palindrome.java
+++ 0409 - Longest Palindrome/0409 - Longest Palindrome.java
@@ -1,8 +1,11 @@
-public class Solution {
-    public int longestPalindrome(String s) {
+public class Solution 
+{
+    public int longestPalindrome(String s) 
+    {
         HashMap<Character, Integer> charFrequency = new HashMap<>();
         int oddFrequencyCount = 0;
-        for (char ch : s.toCharArray()) {
+        for (char ch : s.toCharArray()) 
+        {
             charFrequency.put(ch, charFrequency.getOrDefault(ch, 0) + 1);
             if (charFrequency.get(ch) % 2 == 1)
                 oddFrequencyCount++;
